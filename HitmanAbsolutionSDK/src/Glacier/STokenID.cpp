c94d77dd89b11d7cf0aa72059e066f0c462eb340
Pavle <pavle_nis@yahoo.com>
2023-10-10 17:03:18 +0200

    Fix implementation of STokenID::operator== method

--- HitmanAbsolutionSDK/src/Glacier/STokenID.cpp
+++ HitmanAbsolutionSDK/src/Glacier/STokenID.cpp
@@ -4,12 +4,12 @@ STokenID STokenID::InvalidToken{0, false};
 
 bool STokenID::operator==(const STokenID& a) const
 {
-    if (!m_bValid || !a.m_bValid)
+    if (m_bValid && a.m_bValid)
     {
-        return false;
+        return m_iValue == a.m_iValue;
     }
 
-    return m_iValue == a.m_iValue;
+    return !m_bValid && !a.m_bValid;
 }
 
 bool STokenID::operator!=(const STokenID& a) const
