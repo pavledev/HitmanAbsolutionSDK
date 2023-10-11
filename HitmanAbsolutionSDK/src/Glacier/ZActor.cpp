804cf5ad0658f8abfe398d14871a22616ffd5658
Pavle <pavle_nis@yahoo.com>
2023-10-11 07:23:24 +0200

    Move ZActor to Glacier/Actor

--- HitmanAbsolutionSDK/src/Glacier/ZActor.cpp
+++
@@ -1,9 +0,0 @@
-#include <Glacier/ZActor.h>
-
-#include <Function.h>
-#include <Global.h>
-
-void ZActor::KillActor(EActorDeathType eDeathType, bool bDeathVisible)
-{
-	Function::CallMethod<ZActor*, EActorDeathType, bool>(BaseAddress + 0x174790, this, eDeathType, bDeathVisible);
-}
