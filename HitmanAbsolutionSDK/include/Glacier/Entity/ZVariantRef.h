850c7cd3ac44fdfd11368aa9cca8aae8290b7845
Pavle <pavle_nis@yahoo.com>
2023-11-16 11:02:14 +0100

    Add ZVariantRef constructor

--- HitmanAbsolutionSDK/include/Glacier/Entity/ZVariantRef.h
+++ HitmanAbsolutionSDK/include/Glacier/Entity/ZVariantRef.h
@@ -7,6 +7,7 @@ class ZVariant;
 class HitmanAbsolutionSDK_API ZVariantRef : public ZObjectRef
 {
 public:
+	ZVariantRef();
 	ZVariantRef(const ZVariant& rhs);
 	ZVariantRef(void* pObject, STypeID* const typeID);
 };
--- HitmanAbsolutionSDK/src/Glacier/Entity/ZVariantRef.cpp
+++ HitmanAbsolutionSDK/src/Glacier/Entity/ZVariantRef.cpp
@@ -1,6 +1,10 @@
 #include <Glacier/Entity/ZVariantRef.h>
 #include <Glacier/Entity/ZVariant.h>
 
+ZVariantRef::ZVariantRef()
+{
+}
+
 ZVariantRef::ZVariantRef(const ZVariant& rhs) : ZObjectRef(rhs.GetData(), rhs.GetTypeID())
 {
 }
