85cf75a2e2f60ad5821df6466bc7184218a4e6c5
Pavle <pavle_nis@yahoo.com>
2023-11-17 16:13:41 +0100

    Update TFixedArray.h

--- HitmanAbsolutionSDK/include/Glacier/Templates/TFixedArray.h
+++ HitmanAbsolutionSDK/include/Glacier/Templates/TFixedArray.h
@@ -7,6 +7,33 @@ template <typename TElement, unsigned int TSize>
 class TFixedArray : public TFixedArrayBase<TElement, ZFixedArrayData<TElement, TSize>>
 {
 public:
+    TFixedArray() = default;
+
+    TFixedArray(const TFixedArray& other)
+    {
+        TFixedArray& array = *this;
+
+        for (int i = 0; i < TSize; ++i)
+        {
+            array[i] = other[i];
+        }
+    }
+
+    TFixedArray& operator=(const TFixedArray& other)
+    {
+        if (this != &other)
+        {
+            TFixedArray& array = *this;
+
+            for (int i = 0; i < TSize; ++i)
+            {
+                array[i] = other[i];
+            }
+        }
+
+        return *this;
+    }
+
     unsigned int Size() const
     {
         return TSize;
