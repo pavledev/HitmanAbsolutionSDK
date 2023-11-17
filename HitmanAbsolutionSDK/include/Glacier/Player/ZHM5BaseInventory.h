a03bc7435b9ec194dd2d86b69abd2a45ab0478af
Pavle <pavle_nis@yahoo.com>
2023-11-17 16:13:07 +0100

    Update ZHM5BaseInventory.h

--- HitmanAbsolutionSDK/include/Glacier/Player/ZHM5BaseInventory.h
+++ HitmanAbsolutionSDK/include/Glacier/Player/ZHM5BaseInventory.h
@@ -2,6 +2,7 @@
 
 #include "../Templates/TArray.h"
 #include "../Templates/TEntityRef.h"
+#include "../Templates/TFixedArray.h"
 #include "../Item/ZHM5Item.h"
 
 class ZInventorySlot;
@@ -10,7 +11,10 @@ class HitmanAbsolutionSDK_API ZHM5BaseInventory
 {
 public:
 	ZInventorySlot* AddItemToInventory(const TEntityRef<IHM5Item>& rItem, bool bAddInitialBullets, bool bDisableHUD);
+	void SetAmmoInPocket(TFixedArray<unsigned int, 8>& nAmmoInPocket);
 
 private:
 	TArray<TEntityRef<IHM5Item>> m_Items;
+	TArray<TEntityRef<IHM5Item>> m_RemoteControlledItems;
+	unsigned int m_nAmmoInPocket[8];
 };
