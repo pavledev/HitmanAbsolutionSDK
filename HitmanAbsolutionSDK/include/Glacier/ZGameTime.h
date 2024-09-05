5c9803c89b69c131053afdc8617c3bcde1d38cd1
Pavle <pavle_nis@yahoo.com>
2024-09-05 22:36:09 +0200

    Add ToSeconds to ZGameTime class

--- HitmanAbsolutionSDK/include/Glacier/ZGameTime.h
+++ HitmanAbsolutionSDK/include/Glacier/ZGameTime.h
@@ -6,6 +6,7 @@ class HitmanAbsolutionSDK_API ZGameTime
 {
 public:
     long long GetTicks() const;
+    double ToSeconds() const;
 
 private:
     long long m_nTicks;
--- HitmanAbsolutionSDK/src/Glacier/ZGameTime.cpp
+++ HitmanAbsolutionSDK/src/Glacier/ZGameTime.cpp
@@ -4,3 +4,8 @@ long long ZGameTime::GetTicks() const
 {
 	return m_nTicks;
 }
+
+double ZGameTime::ToSeconds() const
+{
+    return static_cast<double>(m_nTicks) / 1024.0 / 1024.0;
+}
