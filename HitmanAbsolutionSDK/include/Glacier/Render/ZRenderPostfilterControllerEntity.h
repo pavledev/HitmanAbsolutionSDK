d509125e46eac023c292d0e7199a5d4cfdd16b17
Pavle <pavle_nis@yahoo.com>
2024-09-09 15:42:17 +0200

    Create ZRenderPostfilterControllerEntity.h

---
+++ HitmanAbsolutionSDK/include/Glacier/Render/ZRenderPostfilterControllerEntity.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "ZRenderableEntity.h"
+#include "IRenderPostfilterControllerEntity.h"
+
+class ZRenderPostfilterControllerEntity : public ZRenderableEntity, public IRenderPostfilterControllerEntity
+{
+public:
+	ZEntityRef m_PostfilterParametersEntity; //0x64
+	PAD(0x24);
+	ZEntityRef m_CurrentPostfilterParametersEntity; //0x8C
+};
