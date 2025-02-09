9014ae28ae346f1efd1c556fd1c25f8575c7d38a
Pavle <pavle_nis@yahoo.com>
2025-02-09 07:43:11 +0100

    Update ZConfigCommand.h

--- HitmanAbsolutionSDK/include/Glacier/Configuration/ZConfigCommand.h
+++ HitmanAbsolutionSDK/include/Glacier/Configuration/ZConfigCommand.h
@@ -1,6 +1,8 @@
 #pragma once
 
-class ZConfigCommand
+#include "../ZString.h"
+
+class HitmanAbsolutionSDK_API ZConfigCommand
 {
 public:
 	enum ECLASSTYPE
@@ -11,8 +13,13 @@ public:
 		ECLASS_UNKNOWN = 3
 	};
 
-    virtual ECLASSTYPE GetType();
+	virtual ECLASSTYPE GetType() = 0;
     virtual void Execute(const unsigned int param1, const char** param2) = 0;
+
+	static ZConfigCommand* First();
+	ZConfigCommand* Next() const;
+	static void ExecuteCommand(const ZString& pCommandName, const char* argv);
+	ZString Name() const;
 
 protected:
     const char* m_pszName;
