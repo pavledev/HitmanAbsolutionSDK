4c082fd834b907f73075cbae3b7743e30f056c63
Pavle <pavle_nis@yahoo.com>
2026-09-15 22:53:53 +0200

    Remove unsafe DLL cleanup on process detach

--- HitmanAbsolutionSDK/Src/Main.cpp
+++ HitmanAbsolutionSDK/Src/Main.cpp
@@ -20,15 +20,6 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
             CloseHandle(handle);
         }
     }
-    else if (dwReason == DLL_PROCESS_DETACH)
-    {
-        HMODULE module = GetModuleHandleA("HitmanAbsolutionSDK.dll");
-
-        if (module)
-        {
-            FreeLibraryAndExitThread(module, 0);
-        }
-    }
 
     return TRUE;
 }
